72b2f965a873d381949eee2c81cf535df05cf8a2
melody.c000.ron@gmail.com <melody.c000.ron@gmail.com>
2026-03-11 23:21:32 +0800

    建立api資料夾

---
+++ crud-practice/src/api/employeesList..js
@@ -0,0 +1,7 @@
+export async function getProfiles(){}
+
+export async function createProfiles(){}
+
+export async function deleteProfiles(){}
+
+export async function patchProfiles() {}
---
+++ crud-practice/src/data/employeesList..js
@@ -0,0 +1,8 @@
+ const employeeList = [
+    { id: 'E-001', name: 'Jack', role: '工程師', gmail: 'jackie111@heim.com' },
+    { id: 'E-002', name: 'Will', role: '業務', gmail: 'Billy30@heim.com' },
+    { id: 'E-003', name: 'Marry', role: '編輯', gmail: 'Marrygoaround@heim.com' },
+    { id: 'E-004', name: 'Lily', role: '行政', gmail: 'Lily111@heim.com' },
+    { id: 'E-005', name: 'Alex', role: '業務助理', gmail: 'Alexs@heim.com' },
+    { id: 'E-006', name: 'Henry', role: '人資', gmail: 'Henry0566@heim.com' },
+  ]
